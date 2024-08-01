1609e7bbc68db362bf9559e7c8d8221ce7e60445
Asad <asad3gh@gmail.com>
2024-08-01 10:31:03 -0400

    Initial commit

---
+++ src/main/java/com/eaton/kafka/kafkavisiontech/KafkaVisionTechApplication.java
@@ -0,0 +1,13 @@
+package com.eaton.kafka.kafkavisiontech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaVisionTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaVisionTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eaton/kafka/kafkavisiontech/KafkaVisionTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eaton.kafka.kafkavisiontech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaVisionTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
